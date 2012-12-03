e6f318f4b6f3ad5d0a979858e6aa8f3a36e35d15
Alex Pyrgiotis <apyrgio@cslab.ece.ntua.gr>
2012-12-03 15:03:21 +0200

    Major changes to fio_bcache.sh
    Two main changes have occured:
    * Got rid of logging since it was (cool, yet) practically unuseful
    * Added a --wait option. This options stall the benchmarking progress
      as long as it needs for the writeback to finish.
    * Fixed the --dry option

--- scripts/fio_bcache.sh
+++ scripts/fio_bcache.sh
@@ -1,32 +1,44 @@
 #! /bin/bash
 
+txtrst=$(tput sgr0) 	# Reset text color
+txtred=$(tput setaf 1) 	# Make text red
+txtgrn=$(tput setaf 2) 	# Make text green
+
+################## READ ARGUMENTS ###################
+
 print_help(){
-	echo "List of parameters:"
-	echo -e "\t--help, -h:\tprint this message and exit"
-	echo -e "\t--dry:\t\tdo not run any benchmarks"
+	echo -e "\nList of parameters:"
+	echo -e "-h, --help:\tprint this message and exit"
+	echo -e "-d, --dry:\tdo not run any benchmarks"
+	echo -e "-w, --wait:\twait for writeback to finish between each test"
+	exit 0
+}
+
+wait_dirty(){
+	if [[ ! $WAIT ]]; then return; fi
+	echo 1 > /sys/block/$BCACHE_DEV/bcache/writeback_running
+	echo -n "Waiting for bcache to finish writeback... "
+	BACK_STATE="cat /sys/block/${BCACHE_DEV}/bcache/state"
+	while [[ $(eval $BACK_STATE) = "dirty" ]]; do
+		sleep 1
+	done
+	echo "${txtgrn}done.${txtrst}"
 }
 
 while [ -n "$1" ]; do
-	if [[ $1 = "-h" ]] || [[ $1 = "--help" ]]; then
-		print_help
-		exit 0
-	elif [[ $1 = "--dry" ]]; then
-		DRY=true
-	else
-		echo "${1}: Unknown command. Exiting..."
-		exit 1
+	if [[ $1 = "-h" ]] || [[ $1 = "--help" ]]; then print_help
+	elif [[ $1 = "-d" ]] || [[ $1 = "--dry" ]]; then DRY=0
+	elif [[ $1 = "-w" ]] || [[ $1 = "--wait" ]]; then WAIT=0
+	else echo "${txtred}${1}: Unknown command${txtrst}"; print_help
 	fi
 	shift
 done
 
-################### START LOGGING #######################
+####################### PARAMETERS #######################
+
 SCRIPT_NAME=`echo "${0##*/}" | sed 's/.sh//'`
 CUR_DATE=`date +"%d-%m-%y_%T"`
 PROJECT_FOLDER=/home/brainfree/playground/io_tests
-LOGFILE="$PROJECT_FOLDER"/log/"$SCRIPT_NAME"_"$CUR_DATE".log
-( # Parenthesis is VERY important
-
-####################### PARAMETERS #######################
 CACHE_SET=ram0			# The SSD
 BACKING_DEV=sda7		# The RAID
 CACHE_SET_SIZE=2621440	# Cache set size in KB (for ramdisk)
@@ -34,10 +46,6 @@ CACHE_SET_OPTS=
 BACKING_DEV_OPTS=--writeback
 DEVICES_SCRIPT="$PROJECT_FOLDER"/scripts/init.sh
 BCACHE_SCRIPT="$PROJECT_FOLDER"/scripts/do_bcache.sh
-txtrst=$(tput sgr0) 	# Reset text color
-txtred=$(tput setaf 1) 	# Make text red
-txtgrn=$(tput setaf 2) 	# Make text green
-DRY=false
 
 ################# PREPARE AND START BCACHE ###############
 
@@ -54,7 +62,7 @@ fi
 
 ##################### BENCHMARKS ########################
 
-if [ $DRY ]; then
+if [[ $DRY ]]; then
 	echo "Dry run. No benchmark started."
 	exit 0
 fi
@@ -65,6 +73,7 @@ cd /media/bcache
 rm -rf /media/bcache/*
 for IOENGINE in sync libaio ; do
 	for NUMPROCS in 1 2 4 8 ; do
+		wait_dirty	#will do only when -w or --wait option is given
 		SIZE="$[512 / $NUMPROCS]"M
 		export IOENGINE
 		export NUMPROCS
@@ -77,8 +86,3 @@ for IOENGINE in sync libaio ; do
 		chown brainfree:brainfree "$RES_FILE"
 	done
 done
-
-################### STOP LOGGING #####################
-) 2>&1 | tee $LOGFILE
-chown brainfree:brainfree $LOGFILE
-######################################################
